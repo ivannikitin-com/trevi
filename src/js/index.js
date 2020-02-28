78c4f19af7cc6eb06589acf48ed0b935cde7b464
Груздева Светлана <gruzdeva.s@outlook.com>
2020-02-28 14:14:39 +0300

    Главная готова

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+import "bootstrap";
+import "popper.js";
+import $ from "jquery";
+
+$(document).ready(function() {
+  $(".specialists .owl-carousel").owlCarousel({
+    items: 3,
+    nav: true,
+    margin: 78,
+    navText: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      }
+    }
+  });
+
+  $(".slider-main .owl-carousel").owlCarousel({
+    items: 1,
+    nav: false
+  });
+});
