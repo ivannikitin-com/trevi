5e14cfd81e9aa4298f7647dcdba34893af947a71
Груздева Светлана <gruzdeva.s@outlook.com>
2020-03-04 00:09:09 +0300

    Настроена slick

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,14 @@ $(document).ready(function() {
   $(".specialists .carousel").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
+    // appendArrows: $(".specialists__img"),
     responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
       {
         breakpoint: 768,
         settings: {
